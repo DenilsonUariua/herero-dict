efa8ead603d37d9347d843d371241c35e0be3cc0
Denilson Uariua <denuariua@gmail.com>
2025-12-15 12:19:25 +0200

    added comment

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -170,7 +170,7 @@ function toast({ ...props }: Toast) {
 
 function useToast() {
   const [state, setState] = React.useState<State>(memoryState)
-
+// hello world
   React.useEffect(() => {
     listeners.push(setState)
     return () => {
